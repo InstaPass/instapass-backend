d6b73d18ba4b380c6b065b4199609fb812597901
Diving-Fish <dfyshisb@163.com>
2020-06-26 00:19:46 +0800

    update: finish resident and guard

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import binascii
+
 from flask import *
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 import config.dbinfo
 import re
+import os
 from jwt.exceptions import ExpiredSignatureError
 
 app = Flask(__name__)
@@ -19,6 +23,17 @@ db.create_all()
 
 # Tool methods
 
+def is_role(user_id: int, role):
+    user = role.query.filter_by(id=user_id).all()
+    return user
+
+
+def in_community(community_id: int, manage_list):
+    for manage in manage_list:
+        if community_id == manage.community_id:
+            return manage
+    return None
+
 
 def params_not_given():
     return {"status": "err", "msg": "params not given"}, 400
@@ -27,12 +42,29 @@ def params_not_given():
 def valid_login(username: str, password: str):
     user = User.query.filter_by(username=username).first()
     if not user or user.password != password:
-        return False, ""
-    return True, user_id_encode(user.id)
+        return False, "", 0
+    return True, user_id_encode(user.id), user.id
 
 
 def valid_access(dweller: Dweller):
     return True
+
+
+def valid_invitation(key: str):
+    i = InviteKey.query.filter_by(key=key, used=False).first()
+    if i:
+        i.used = True
+        db.session.commit()
+        return i.community_id
+    else:
+        return -1
+
+
+def get_invitation(id: int):
+    r = str(binascii.hexlify(os.urandom(16)), encoding='utf-8')
+    db.session.add(InviteKey(community_id=id, key=r, used=False))
+    db.session.commit()
+    return r
 
 
 # Decorators
@@ -49,6 +81,8 @@ def login_required(f):
             return {"status": "error", "msg": "You're not login"}, 401
         if token['exp'] < ts():
             return {"status": "error", "msg": "Login expired"}, 401
+        g.id = token['id']
+        g.user = User.query.filter_by(id=g.id).first()
         return f(*args, **kwargs)
 
     return decorated_function
@@ -59,9 +93,9 @@ def dweller_required(f):
     def decorated_function(*args, **kwargs):
         try:
             token = decode(request.headers['Jwt-Token'])
-            dweller = Dweller.query.filter_by(id=token['id']).first()
+            dweller = is_role(token['id'], Dweller)
             if not dweller:
-                return {"status": "error", "msg": "You're not a dweller"}, 400
+                return {"status": "error", "msg": "You're not a dweller"}, 403
         except KeyError:
             return {"status": "error", "msg": "You're not login"}, 401
         if token == {}:
@@ -69,6 +103,7 @@ def dweller_required(f):
         if token['exp'] < ts():
             return {"status": "error", "msg": "Login expired"}, 401
         g.dweller = dweller
+        g.id = token['id']
         return f(*args, **kwargs)
 
     return decorated_function
@@ -79,9 +114,9 @@ def guard_required(f):
     def decorated_function(*args, **kwargs):
         try:
             token = decode(request.headers['Jwt-Token'])
-            guard = Monitor.query.filter_by(id=token['id']).first()
+            guard = is_role(token["id"], Monitor)
             if not guard:
-                return {"status": "error", "msg": "You're not a dweller"}, 400
+                return {"status": "error", "msg": "You're not a guard"}, 403
         except KeyError:
             return {"status": "error", "msg": "You're not login"}, 401
         if token == {}:
@@ -89,6 +124,28 @@ def guard_required(f):
         if token['exp'] < ts():
             return {"status": "error", "msg": "Login expired"}, 401
         g.guard = guard
+        g.id = token['id']
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            token = decode(request.headers['Jwt-Token'])
+            admin = is_role(token["id"], Admin)
+            if not admin:
+                return {"status": "error", "msg": "You're not an admin"}, 403
+        except KeyError:
+            return {"status": "error", "msg": "You're not login"}, 401
+        if token == {}:
+            return {"status": "error", "msg": "You're not login"}, 401
+        if token['exp'] < ts():
+            return {"status": "error", "msg": "Login expired"}, 401
+        g.admin = admin
+        g.id = token['id']
         return f(*args, **kwargs)
 
     return decorated_function
@@ -103,25 +160,170 @@ def need_login_test():
     return {"msg": "you're login"}
 
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/<role>/login', methods=['POST'])
+def login(role):
+    role_table = {"admin": Admin, "guard": Monitor, "resident": Dweller}
     try:
-        success, token = valid_login(request.json['username'], request.json['password'])
+        success, token, user_id = valid_login(request.json['username'], request.json['password'])
         if success:
-            return {"jwt_token": token}
+            try:
+                if role != "resident" and not is_role(user_id, role_table[role]):
+                    return {"status": "error", "msg": f"You're not {role}"}, 403
+                else:
+                    return {"jwt_token": token}
+            except KeyError:
+                return {"status": "error", "msg": f"No role named {role}"}, 403
         else:
             return {"status": "err", "msg": "Wrong username or password"}, 401
     except KeyError:
         return params_not_given()
 
 
-@app.route('/resident/qrcode', methods=['GET'])
+@app.route('/resident/qrcode/<int:id>', methods=['GET'])
 @dweller_required
-def get_qrcode():
-    return {"status": "ok",
-            "last_refresh_time": ts(),
-            "secret": "instapass{%s}" % qrcode_encode(g.dweller.id, g.dweller.community_id)}
+def get_qrcode(id):
+    if in_community(id, g.dweller):
+        return {"status": "ok",
+                "last_refresh_time": ts(),
+                "secret": "instapass{%s}" % qrcode_encode(g.dweller[0].id, id)}
+    else:
+        return {"status": "error"}, 403
 
+
+@app.route('/resident/community', methods=['GET'])
+@dweller_required
+def get_community_info():
+    communities = []
+    community_map = {}
+    for d in g.dweller:
+        community_map[d.community_id] = Community.query.filter_by(id=d.community_id).first()
+    for c in community_map:
+        communities.append({
+            "community_id": community_map[c].id,
+            "community": community_map[c].name,
+            "address": community_map[c].address
+        })
+    return {"status": "ok", "communities": communities}
+
+
+@app.route('/resident/community/enter', methods=['POST'])
+@login_required
+def enter_community():
+    try:
+        invitation_code = request.json['invitation_code']
+        community_id = valid_invitation(invitation_code)
+        if community_id == -1:
+            return {"status": "error"}, 400
+        else:
+            if Dweller.query.filter_by(id=g.id, community_id=community_id).first():
+                return {"status": "error", "message": "you have entered this community"}, 400
+            db.session.add(Dweller(id=g.id, community_id=community_id, last_access_time=0))
+            db.session.commit()
+            community = Community.query.filter_by(id=community_id).first()
+            return {"status": "ok", "community_id": community_id, "community": community.name}
+    except KeyError:
+        return params_not_given()
+
+
+@app.route('/resident/community/leave', methods=['POST'])
+@dweller_required
+def leave_community():
+    try:
+        community_id = request.json['community_id']
+        if community_id == -1:
+            return {"status": "error"}, 400
+        else:
+            db.session.delete(Dweller.query.filter_by(id=g.id, community_id=community_id).first())
+            db.session.commit()
+            return {"status": "ok"}
+    except KeyError:
+        return params_not_given()
+
+
+@app.route('/resident/notifications', methods=['GET'])
+@dweller_required
+def resident_retrieve_notifications():
+    is_all = request.args.get("all")
+    user = User.query.filter_by(id=g.id).first()
+    communities = []
+    community_map = {}
+    for dweller in g.dweller:
+        communities.append(dweller.community_id)
+        community_map[dweller.community_id] = Community.query.filter_by(id=dweller.community_id).first()
+    last_retrieve_time = user.last_retrieve_time
+    user.last_retrieve_time = ts()
+    notifications = []
+    if is_all is None:
+        notices = db.session.query(Notice).filter(and_(
+            Notice.create_time >= last_retrieve_time, Notice.community_id.in_(communities))).all()
+    else:
+        notices = db.session.query(Notice).filter(Notice.community_id.in_(communities)).all()
+    for notice in notices:
+        notifications.append({
+            "community_id": notice.community_id,
+            "community": community_map[notice.community_id].name,
+            "address": community_map[notice.community_id].address,
+            "content": notice.content,
+            "author": notice.author
+        })
+    db.session.commit()
+    return {"status": "ok", "last_retrieve_time": user.last_retrieve_time, "notifications": notifications}
+
+
+@app.route('/resident/history/<int:id>', methods=['GET'])
+@dweller_required
+def get_history(id):
+    dweller = in_community(id, g.dweller)
+    community = Community.query.filter_by(id=id).first()
+    logs = Log.query.filter_by(community_id=id, user_id=g.id).all()
+    if not dweller:
+        return {"status": "error"}, 403
+    else:
+        log_json = []
+        for log in logs:
+            log_json.append({
+                "community_id": id,
+                "community": community.name,
+                "address": community.address,
+                "time": log.access_time,
+                "reason": log.note,
+            })
+        return {
+            "status": "ok",
+            "current_status": "inside" if dweller.inside else "outside",
+            "last_exit_time": dweller.last_access_time,
+            "history": log_json
+        }
+
+
+@app.route('/resident/info', methods=['GET', 'POST'])
+@login_required
+def get_info():
+    if request.method == 'GET':
+        return {
+            "status": "ok",
+            "variable_info": {
+                "nickname": g.user.nickname,
+                "phone_no": g.user.phone,
+                "mail_address": g.user.email_address
+            },
+            "static_info": {
+                "realname": g.user.name,
+                "id_number": g.user.id_number
+            }
+        }
+    else:
+        try:
+            g.user.nickname = request.json["variable_info"]["nickname"]
+            g.user.phone = request.json["variable_info"]["phone_no"]
+            g.user.email_address = request.json["variable_info"]["mail_address"]
+            db.session.commit()
+            return {"status": "ok"}
+        except KeyError:
+            return params_not_given()
+
+
+# Guard
 
 @app.route('/guard/validate', methods=['POST'])
 @guard_required
@@ -131,11 +333,13 @@ def validate_qrcode():
         secret = request.json['secret']
         jwt_secret = re.match("instapass{(.*)}", secret).groups()[0]
         qr_json = decode(jwt_secret)
-        if qr_json['community_id'] == g.guard.community_id:
+        guard = in_community(qr_json['community_id'], g.guard)
+        if guard and guard.working_until > ts():
             db.session.add(Log(community_id=qr_json['community_id'], user_id=qr_json['id'], temperature=37.0,
                                note=reason, access_time=ts()))
             dweller = Dweller.query.filter_by(id=qr_json['id']).first()
             dweller.last_access_time = ts()
+            dweller.inside = not dweller.inside
             db.session.commit()
             return {"status": "ok", "validation": "accepted"}
         else:
@@ -144,6 +348,97 @@ def validate_qrcode():
         return params_not_given()
     except ExpiredSignatureError:
         return {"status": "err", "msg": "qrcode has expired"}, 400
+
+
+@app.route("/guard/checkin", methods=['POST'])
+@guard_required
+def guard_checkin():
+    try:
+        community_id = request.json["community_id"]
+        guard = in_community(community_id, g.guard)
+        if guard:
+            guard.working_until = ts(12 * 3600)
+            db.session.commit()
+            return {"status": "ok"}
+        else:
+            return {"status": "err", "msg": "permission denied"}, 403
+    except KeyError:
+        return params_not_given()
+
+
+@app.route("/guard/checkout", methods=['POST'])
+@guard_required
+def guard_checkout():
+    try:
+        community_id = request.json["community_id"]
+        guard = in_community(community_id, g.guard)
+        if guard:
+            guard.working_until = 0
+            db.session.commit()
+            return {"status": "ok"}
+        else:
+            return {"status": "err", "msg": "permission denied"}, 403
+    except KeyError:
+        return params_not_given()
+
+
+@app.route('/admin/notify/release', methods=['POST'])
+@admin_required
+def release_notification():
+    try:
+        community_id = request.json["notification"]["community_id"]
+        author = request.json["notification"]["author"]
+        content = request.json["notification"]["content"]
+        community = Community.query.filter_by(id=community_id).first()
+        if not community or not in_community(community_id, g.admin):
+            return {"status": "err", "msg": "permission denied"}, 403
+        db.session.add(Notice(community_id=community_id, author=author, content=content, create_time=ts(),
+                              sender_id=g.admin[0].id))
+        db.session.commit()
+        return {"status": "ok"}
+    except KeyError:
+        return params_not_given()
+
+# Admin
+
+
+@app.route('/admin/notifications', methods=['GET'])
+@admin_required
+def admin_retrieve_notifications():
+    is_all = request.args.get("all")
+    user = User.query.filter_by(id=g.id).first()
+    communities = []
+    community_map = {}
+    for admin in g.admin:
+        communities.append(admin.community_id)
+        community_map[admin.community_id] = Community.query.filter_by(id=admin.community_id).first()
+    last_retrieve_time = user.last_retrieve_time
+    user.last_retrieve_time = ts()
+    notifications = []
+    if is_all is None:
+        notices = db.session.query(Notice).filter(and_(
+            Notice.create_time >= last_retrieve_time, Notice.community_id.in_(communities))).all()
+    else:
+        notices = db.session.query(Notice).filter(Notice.community_id.in_(communities)).all()
+    for notice in notices:
+        notifications.append({
+            "community_id": notice.community_id,
+            "community": community_map[notice.community_id].name,
+            "address": community_map[notice.community_id].address,
+            "content": notice.content,
+            "author": notice.author
+        })
+    db.session.commit()
+    return {"status": "ok", "last_retrieve_time": user.last_retrieve_time, "notifications": notifications}
+
+
+@app.route('/admin/invite/<int:id>', methods=['GET'])
+@admin_required
+def admin_invite(id):
+    if in_community(id, g.admin):
+        return {"key": get_invitation(id)}
+    else:
+        return {"status": "error"}, 403
 
 
 @app.errorhandler(405)
--- models/models.py
+++ models/models.py
@@ -9,6 +9,12 @@ class User(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(32))
     password = db.Column('password', db.String(32))
+    last_retrieve_time = db.Column('last_retrieve_time', db.Integer)
+    name = db.Column('real_name', db.String(32))
+    id_number = db.Column('id_number', db.String(32))
+    nickname = db.Column('nickname', db.String(32))
+    phone = db.Column('phone', db.String(32))
+    email_address = db.Column('email_address', db.String(64))
 
 
 class Community(db.Model):
@@ -23,26 +29,26 @@ class Family(db.Model):
 
 
 class Dweller(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
+    auto = db.Column('auto', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer)
     community_id = db.Column('community_id', db.Integer)
-    name = db.Column('real_name', db.String(32))
-    id_number = db.Column('id_number', db.String(32))
-    phone = db.Column('phone', db.String(32))
-    email_address = db.Column('email_address', db.String(64))
     family_id = db.Column('family_id', db.Integer)
     last_access_time = db.Column('last_access_time', db.Integer)
+    inside = db.Column('inside', db.Boolean)
     face_data = db.String('face_data', db.String(1 << 15))
     health_status = db.Column('health_status', db.Integer)
 
 
 class Admin(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
+    auto = db.Column('auto', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer)
     community_id = db.Column('community_id', db.Integer)
     level = db.Column('level', db.Integer)
 
 
 class Monitor(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
+    auto = db.Column('auto', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer)
     community_id = db.Column('community_id', db.Integer)
     working_until = db.Column('working_until', db.Integer)
 
@@ -51,8 +57,9 @@ class Notice(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     community_id = db.Column('community_id', db.Integer)
     sender_id = db.Column('sender_id', db.Integer)
-    title = db.String('title', db.String(64))
+    author = db.Column('author', db.String(64))
     content = db.Column('content', db.String(1 << 12))
+    create_time = db.Column("create_time", db.Integer)
 
 
 class Log(db.Model):
@@ -62,3 +69,10 @@ class Log(db.Model):
     access_time = db.Column('access_time', db.Integer)
     temperature = db.Column('temperature', db.Float)
     note = db.Column('note', db.String(128))
+
+
+class InviteKey(db.Model):
+    auto = db.Column('auto', db.Integer, primary_key=True)
+    community_id = db.Column('community_id', db.Integer)
+    key = db.Column('key', db.String(64))
+    used = db.Column('used', db.Boolean)
